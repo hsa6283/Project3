adc97383479cdfb977213a542347191b001eca0d
H <80310260+hsa6283@users.noreply.github.com>
2023-11-14 21:00:40 -0600

    main animation

--- script.js
+++ script.js
@@ -24,4 +24,18 @@ function initMap() {
     infowindow.open(map, marker);
   }
   window.initMap = initMap;
+
+  var string = "Introduce Myself!";
+  var array = string.split("");
+  var timer;
+
+function frameLooper () {
+	if (array.length > 0) {
+		document.getElementById("text").innerHTML += array.shift();
+	} else {
+		clearTimeout(timer);
+			}
+	loopTimer = setTimeout('frameLooper()',70);
+  }
+frameLooper();
   
